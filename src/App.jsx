e375eb3da4a936eac328f12d4e041df5d94efa31
kidstone <hyuksu0308@naver.com>
2024-02-27 20:10:37 +0900

    FIX 아이콘 링크 삽입

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import './App.css';
 import React from 'react';
-import { RouterProvider, createBrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import OnboardingPage from './pages/OnboardingPage';
 import LoginPage from './pages/LoginPage';
 import RoommatePage from './pages/RoommatePage';
 import HomePage from './pages/HomePage';
 import Navbar from './components/common/Navbar';
-const router = createBrowserRouter([
-  { path: "/", Component: LandingPage },
-  { path: "/onboarding", Component: OnboardingPage },
-  { path: "/login", Component: LoginPage },
-  { path: "/home", Component: HomePage },
-  { path: "/roommate", Component: RoommatePage },
-]);
+
 function App() {
   const path = window.location.pathname
+
   return (
     <div className="App">
-      <RouterProvider router={router} />
-      {(path !== '/' && path !== '/onboarding') && <Navbar />}
+      <Router>
+        {(path !== '/' && path !== '/onboarding') && <Navbar />}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/onboarding" element={<OnboardingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/roommate" element={<RoommatePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import ChatIcon from '../../assets/images/chat.svg'
 import RoommateIcon from '../../assets/images/roommateIcon.svg'
 import HomeIcon from '../../assets/images/home-icon.svg'
@@ -7,33 +8,33 @@ import MypageIcon from '../../assets/images/user-icon.svg'
 
 const Navbar = () => {
   return (
-    <div className='flex absolute bottom-0 justify-around items-center h-[66px] bg-white text-[#707070] text-[10px] w-full'>
-          <div className='flex flex-col items-center'>
+    <div className='z-10 flex absolute bottom-0 justify-around items-center h-[66px] bg-white text-[#707070] text-[10px] w-full'>
+          <Link to='#' className='flex flex-col items-center'>
             <div className='h-[27px]'>
               <img src={ChatIcon}/>
             </div>
             <span>채팅</span>
-          </div>
-          <div className='flex flex-col items-center'>
-          <div className='h-[27px]'>
+          </Link>
+          <Link to='/roommate' className='flex flex-col items-center'>
+            <div className='h-[27px]'>
               <img src={RoommateIcon}/>
             </div>
             <span>룸메이트</span>
-          </div>
-          <div className='flex flex-col items-center'>
-          <div className='h-[27px]'>
+          </Link>
+          <Link to='/home' className='flex flex-col items-center'>
+            <div className='h-[27px]'>
               <img src={HomeIcon}/>
             </div>
             <span>홈</span>
-          </div>
+          </Link>
           <div className='flex flex-col items-center'>
-          <div className='h-[27px]'>
+            <div className='h-[27px]'>
               <img src={DeliveryIcon}/>
             </div>
             <span>공동배달</span>
           </div>
           <div className='flex flex-col items-center'>
-          <div className='h-[27px]'>
+            <div className='h-[27px]'>
               <img src={MypageIcon}/>
             </div>
             <span>마이페이지</span>
