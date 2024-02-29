3860f5f5d1e9618ae42edbbdb740c438dbb7db2a
kye1115z <kye1115z@naver.com>
2024-03-01 03:12:29 +0900

    chore: 컬러 설정

---
+++ src/styles/color.js
@@ -0,0 +1,16 @@
+const COLOR = {
+	black: '#000000',
+	white: '#FFFFFF',
+	red: '#F9595F',
+	gray70: '#707070',
+	grayDE: '#DEDEDE',
+	grayF3: '#F3F5F7',
+	purple7F: '#7F6FD8',
+	purpleMAIN: '#8A7FF9',
+	purpleA3: '#A39AEB',
+	purpleEB: '#EBDBFF',
+	purpleF2: '#F2F2FD',
+	purpleBE: '#BEBBCF',
+};
+
+export default COLOR;
