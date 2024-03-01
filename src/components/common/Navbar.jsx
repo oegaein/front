85da62532589055da9b8fdff922ab7e2da6d0698
kidstone <hyuksu0308@naver.com>
2024-03-01 15:19:05 +0900

    Fix: Navbar 경로 로직 변경

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import ChatIcon from '../../assets/images/chat.svg'
 import ChatIconClicked from '../../assets/images/chat-icon-clicked.svg'
 import RoommateIcon from '../../assets/images/roommateIcon.svg'
@@ -13,36 +13,36 @@ import MypageIconClicked from '../../assets/images/user-icon-clicked.svg'
 
 
 const Navbar = () => {
-  const [activeLink, setActiveLink] = useState('home')
+  const location = useLocation()
   return (
     <div className='z-10 flex sticky bottom-0 right-0 left-0 justify-around items-center h-[66px] bg-white text-[#707070] text-[10px] w-full'>
-          <Link to='#' className='flex flex-col items-center' onClick={()=>setActiveLink('chat')}>
+          <Link to='#' className='flex flex-col items-center'>
             <div className='h-[27px]'>
-              <img src={activeLink === 'chat' ? ChatIconClicked : ChatIcon}/>
+              <img src={location.pathname === '/chat' ? ChatIconClicked : ChatIcon}/>
             </div>
             <span>채팅</span>
           </Link>
-          <Link to='/roommate' className='flex flex-col items-center' onClick={()=>setActiveLink('roommate')}>
+          <Link to='/roommate' className='flex flex-col items-center'>
             <div className='h-[27px]'>
-              <img src={activeLink === 'roommate' ? RoommateIconClicked : RoommateIcon}/>
+              <img src={location.pathname === '/roommate' ? RoommateIconClicked : RoommateIcon}/>
             </div>
             <span>룸메이트</span>
           </Link>
-          <Link to='/home' className='flex flex-col items-center' onClick={()=>setActiveLink('home')}>
+          <Link to='/home' className='flex flex-col items-center'>
             <div className='h-[27px]'>
-              <img src={activeLink === 'home' ? HomeIconClicked : HomeIcon}/>
+              <img src={location.pathname === '/home' ? HomeIconClicked : HomeIcon}/>
             </div>
             <span>홈</span>
           </Link>
-          <Link className='flex flex-col items-center' onClick={()=>setActiveLink('delivery')}>
+          <Link className='flex flex-col items-center'>
             <div className='h-[27px]'>
-              <img src={activeLink === 'delivery' ? DeliveryIconClicked : DeliveryIcon}/>
+              <img src={location.pathname === '/delivery' ? DeliveryIconClicked : DeliveryIcon}/>
             </div>
             <span>공동배달</span>
           </Link>
-          <Link to='/mypage' className='flex flex-col items-center' onClick={()=>setActiveLink('mypage')}>
+          <Link to='/mypage' className='flex flex-col items-center'>
             <div className='h-[27px]'>
-              <img src={activeLink === 'mypage' ? MypageIconClicked : MypageIcon}/>
+              <img src={location.pathname === '/mypage' ? MypageIconClicked : MypageIcon}/>
             </div>
             <span>마이페이지</span>
           </Link>
