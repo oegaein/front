c7396745dcd2b2cecedc91577fdd1b1b5ae19531
kye1115z <kye1115z@naver.com>
2024-03-03 02:47:11 +0900

    docs: BasicSettingInput 모음 파일

---
+++ src/components/basicInfo/BasicSettingInput.jsx
@@ -0,0 +1,31 @@
+export const BasicInput = () => {
+	return (
+		<>
+			<div>BasicInput</div>
+		</>
+	);
+};
+
+export const NumInput = () => {
+	return (
+		<>
+			<div>NumInput</div>
+		</>
+	);
+};
+
+export const RadioInput = () => {
+	return (
+		<>
+			<div>RadioInput</div>
+		</>
+	);
+};
+
+export const CheckboxInput = () => {
+	return (
+		<>
+			<div>CheckboxInput</div>
+		</>
+	);
+};
