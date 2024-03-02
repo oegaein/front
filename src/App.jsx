a81f7692ce3d7f5eb970bd0625c52e2ac98efe00
kye1115z <kye1115z@naver.com>
2024-03-02 20:10:39 +0900

    docs: router 삭제 후 되돌려놓기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css';
 import React from 'react';
-import Router from 'router/Router';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from '@pages/LandingPage';
+import OnboardingPage from '@pages/OnboardingPage';
+import BasicInfoLanding from '@pages/basicInfo/BasicInfoLanding';
 
-function App() {
+function Router() {
 	return (
 		<div className="App">
-			<Router />
+			<React.StrictMode>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<LandingPage />} />
+						<Route path="/onboarding" element={<OnboardingPage />} />
+						<Route path="/basicinfo/landing" element={<BasicInfoLanding />} />
+					</Routes>
+				</BrowserRouter>
+			</React.StrictMode>
 		</div>
 	);
 }
 
-export default App;
+export default Router;
