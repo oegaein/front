fac09fd567261149bf86f9d6cc7c520e3f8277fb
kye1115z <kye1115z@naver.com>
2024-03-05 01:45:11 +0900

    docs: Subtitle 태그, 좌측 정렬 변경

--- src/styles/basicInfo/Text.jsx
+++ src/styles/basicInfo/Text.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
-export const Subtitle = styled.div`
+export const Subtitle = styled.span`
+	text-align: left;
 	margin-bottom: 15px;
 	width: 100%;
-	justify-content: flex-start;
-	align-items: flex-start;
 	font-size: 14px;
 `;
