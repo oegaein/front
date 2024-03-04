40e596d698ce6a41096253375a7234e3851c834b
kye1115z <kye1115z@naver.com>
2024-03-05 01:31:43 +0900

    feat: BasicInput 생성

--- src/components/basicInfo/BasicSettingInput.jsx
+++ src/components/basicInfo/BasicSettingInput.jsx
@@ -1,11 +1,35 @@
-export const BasicInput = () => {
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+//
+export const BasicInput = ({ onChangeValue, limitNum }) => {
+	const [value, setValue] = useState('');
+
+	const handleChange = (e) => {
+		const inputValue = e.target.value;
+		if (inputValue.length <= limitNum) {
+			setValue(inputValue);
+			onChangeValue(inputValue);
+		}
+	};
+
 	return (
 		<>
-			<div>BasicInput</div>
+			<input
+				className="w-4/5 pl-1 focus:outline-none"
+				type="text"
+				value={value}
+				onChange={handleChange}
+			/>
 		</>
 	);
 };
 
+BasicInput.propTypes = {
+	onChangeValue: PropTypes.func.isRequired,
+};
+
+//
 export const NumInput = () => {
 	return (
 		<>
@@ -14,6 +38,7 @@ export const NumInput = () => {
 	);
 };
 
+//
 export const RadioInput = () => {
 	return (
 		<>
@@ -22,6 +47,7 @@ export const RadioInput = () => {
 	);
 };
 
+//
 export const CheckboxInput = () => {
 	return (
 		<>
