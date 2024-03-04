7faacf7ec52115d747b837e0ce95625d55ed1f2f
kye1115z <kye1115z@naver.com>
2024-03-05 01:02:50 +0900

    docs: Text 스타일 생성

---
+++ src/styles/basicInfo/Text.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Subtitle = styled.div`
+	margin-bottom: 15px;
+	width: 100%;
+	justify-content: flex-start;
+	align-items: flex-start;
+	font-size: 14px;
+`;
