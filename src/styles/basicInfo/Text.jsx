a2932679c39fa212081d6b5c1aaade595c67dfcc
kye1115z <kye1115z@naver.com>
2024-03-08 01:45:44 +0900

    feat: UnderMsg 스타일 추가

--- src/styles/basicInfo/Text.jsx
+++ src/styles/basicInfo/Text.jsx
@@ -1,9 +1,15 @@
+import COLOR from '@styles/color';
+import FONT from '@styles/fonts';
 import styled from 'styled-components';
 
 export const Subtitle = styled.span`
+	font: ${FONT.body4SB15};
 	text-align: left;
 	margin-bottom: 15px;
 	width: 100%;
-	font-size: 15px;
-	font-weight: 500;
+`;
+
+export const UnderMsg = styled.p`
+	font: ${FONT.caption2M14};
+	color: ${COLOR.gray500};
 `;
