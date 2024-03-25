94ac86163d00629db7fcfa4792575d3734797c1e
kidstone <hyuksu0308@naver.com>
2024-03-25 19:44:33 +0900

    Feat: NotificationPage, Notification 컴포넌트 마크업 완료

--- src/pages/NotificationPage/NotificationPage.jsx
+++ src/pages/NotificationPage/NotificationPage.jsx
@@ -1,33 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import FONT from '@styles/fonts'
 import COLOR from '@styles/color'
+import Notification from '@common/NotificationPage/Notification'
 
-import ProfileImage from '@assets/images/ProfilePhoto.svg'
-import Close from '@assets/images/close.svg'
 const NotificationPage = () => {
+  const [selectedTitle, setSelectedTitle] = useState('roommate')
+  const handleClick = (type) => {
+    setSelectedTitle(type)
+  }
   return (
     <SettingStyle>
       <div className='flex'>
-        <div className='notification-title selected'>룸메이트</div>
-        <div className='notification-title'>공동배달</div>
-        <div className='notification-title'>기숙사소식</div>
+        <div onClick={()=>handleClick('roommate')}className={`notification-title ${selectedTitle === 'roommate' && 'selected-title'}`}>룸메이트</div>
+        <div onClick={()=>handleClick('delivery')}className={`notification-title ${selectedTitle === 'delivery' && 'selected-title'}`}>공동배달</div>
+        <div onClick={()=>handleClick('dorm')}className={`notification-title ${selectedTitle === 'dorm' && 'selected-title'}`}>기숙사소식</div>
       </div>
-      <div className='pb-[4px]'>
-        <div className='flex justify-between px-[24px] py-[12px]'>
-          <div className='flex'>
-            <div>
-              <img src={ProfileImage} className='w-[65px] h-[65px]' alt='profile'/>
-            </div>
-            <div className='pt-[7px] ml-[10px] text-left'>
-              <p className='notification-contents'>차은우님이 룸메이트 신청을 보냈어요</p>
-              <span className='notification-time'>30분전</span>
-            </div>
-          </div>
-          <div className='pt-[9px]'>
-            <img src={Close} className='h-[11px] w-[11px]' alt='close button'/>
-          </div>
-        </div>
+      <div className='pt-[4px]'>
+        {/*Show NoResults or Notification Data */}
+        <NoResults/>
+        <Notification/>
       </div>
     </SettingStyle>
   )
@@ -42,16 +34,25 @@ const SettingStyle = styled.div`
     color: ${COLOR.gray500};
     border-bottom: 2px solid ${COLOR.gray100};
     padding-bottom: 9px;
+    cursor: pointer;
+    &:hover {
+      border-bottom: 2px solid ${COLOR.purple1};
+      color: black;
+    }
   }
-  .selected {
+  .selected-title {
     border-bottom: 2px solid ${COLOR.purple1};
     color: black;
   }
-  .notification-contents {
-
-  }
-  .notification-time {
-    
+  .noresults {
+    font-size: ${FONT.caption2M14};
+    color: ${COLOR.gray500};
   }
 
 `
+
+const NoResults = () => {
+  return (
+    <p className='noresults mt-[141px]'>새로운 소식이 없어요.</p>
+  )
+}
