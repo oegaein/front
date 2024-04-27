443bf43da10be9f8c83385d7eff58e9be4649649
YeEun Kim <kye1115z@naver.com>
2024-04-28 02:48:41 +0900

    chore: Protypes 삭제

--- src/components/common/button/BasicButton.jsx
+++ src/components/common/button/BasicButton.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 import COLOR from '@styles/color';
 import FONT from '@styles/fonts';
 
