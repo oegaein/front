c39aad4631f2a88071c1ad2d97c4eeb3cdc4a349
YeEun Kim <kye1115z@naver.com>
2024-05-16 00:12:34 +0900

    chore: API 주소 변경

--- src/services/api/MatchingRequestAPI.jsx
+++ src/services/api/MatchingRequestAPI.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const postMatchingRequestAPI = async (id) => {
 	const { data } = await axios.post(
-		`http://34.64.159.144:8080/api/v1/matchingrequests`,
+		`http://34.64.153.69:8080/api/v1/matchingrequests`,
 		id,
 	);
 	return data;
