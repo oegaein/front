57020ab2c677bf18642fab3376d0eda1ed55ebaf
YeEun Kim <kye1115z@naver.com>
2024-05-30 23:59:13 +0900

    feat: AlarmAPI 생성

---
+++ src/services/api/AlarmAPI.jsx
@@ -0,0 +1,110 @@
+import useAuthStore from '@store/authStore';
+import { API } from '@utils/api';
+
+export const getAlarmAPI = async (setAccessToken) => {
+	try {
+		const { data } = await API.get(
+			`https://api.oegaein.com:8080/api/v1/roommate-alarms`,
+		);
+		return data;
+	} catch (error) {
+		console.error(error);
+		if (error.response && error.response.status === 403) {
+			try {
+				const refreshResponse = await API.get(`/api/v1/member/refresh`);
+				console.log('refresh!!!!!!' + refreshResponse.data.access_token);
+				setAccessToken(refreshResponse.data.access_token);
+				const accessToken = useAuthStore.getState().accessToken;
+				console.log(accessToken);
+				try {
+					const { data } = await API.get(
+						`https://api.oegaein.com:8080/api/v1/roommate-alarms`,
+						{
+							headers: {
+								Authorization: `Bearer ${accessToken}`,
+							},
+						},
+					);
+					return data;
+				} catch (error) {
+					console.log('error');
+					console.error(error);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
+};
+
+export const deleteAllAlarmAPI = async (setAccessToken) => {
+	try {
+		const { data } = await API.delete(
+			`https://api.oegaein.com:8080/api/v1/roommate-alarms`,
+		);
+		return data;
+	} catch (error) {
+		console.error(error);
+		if (error.response && error.response.status === 403) {
+			try {
+				const refreshResponse = await API.get(`/api/v1/member/refresh`);
+				console.log('refresh!!!!!!' + refreshResponse.data.access_token);
+				setAccessToken(refreshResponse.data.access_token);
+				const accessToken = useAuthStore.getState().accessToken;
+				console.log(accessToken);
+				try {
+					const { data } = await API.delete(
+						`https://api.oegaein.com:8080/api/v1/roommate-alarms`,
+						{
+							headers: {
+								Authorization: `Bearer ${accessToken}`,
+							},
+						},
+					);
+					return data;
+				} catch (error) {
+					console.log('error');
+					console.error(error);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
+};
+
+export const deleteAlarmAPI = async (id, setAccessToken) => {
+	try {
+		const { data } = await API.delete(
+			`https://api.oegaein.com:8080/api/v1/roommate-alarms/${id}`,
+		);
+		return data;
+	} catch (error) {
+		console.error(error);
+		if (error.response && error.response.status === 403) {
+			try {
+				const refreshResponse = await API.get(`/api/v1/member/refresh`);
+				console.log('refresh!!!!!!' + refreshResponse.data.access_token);
+				setAccessToken(refreshResponse.data.access_token);
+				const accessToken = useAuthStore.getState().accessToken;
+				console.log(accessToken);
+				try {
+					const { data } = await API.delete(
+						`https://api.oegaein.com:8080/api/v1/roommate-alarms`,
+						{
+							headers: {
+								Authorization: `Bearer ${accessToken}`,
+							},
+						},
+					);
+					return data;
+				} catch (error) {
+					console.log('error');
+					console.error(error);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
+};
