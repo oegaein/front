ac34310b08a0605f886f3b38a80a9bf48a9b7dae
YeEun Kim <kye1115z@naver.com>
2024-05-31 00:00:04 +0900

    feat: 매칭 신청 api 생성

--- src/services/api/MatchingRequestAPI.jsx
+++ src/services/api/MatchingRequestAPI.jsx
@@ -1,9 +1,42 @@
+import useAuthStore from '@store/authStore';
+import { API } from '@utils/api';
 import axios from 'axios';
 
-export const postMatchingRequestAPI = async (id) => {
-	const { data } = await axios.post(
-		`http://34.64.153.69:8080/api/v1/matchingrequests`,
-		id,
-	);
-	return data;
+export const postMatchingRequestAPI = async (id, setAccessToken) => {
+	try {
+		const { data } = await API.post(
+			`https://api.oegaein.com:8080/api/v1/matchingrequests`,
+			{ matchingPostId: id },
+		);
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error(error);
+		if (error.response && error.response.status === 403) {
+			try {
+				const refreshResponse = await API.get(`/api/v1/member/refresh`);
+				console.log('refresh!!!!!!' + refreshResponse.data.access_token);
+				setAccessToken(refreshResponse.data.access_token);
+				const accessToken = useAuthStore.getState().accessToken;
+				console.log(accessToken);
+				try {
+					const { data } = await API.post(
+						`https://api.oegaein.com:8080/api/v1/matchingrequests`,
+						{
+							matchingPostId: id,
+							headers: {
+								Authorization: `Bearer ${accessToken}`,
+							},
+						},
+					);
+					return data;
+				} catch (error) {
+					console.log('error');
+					console.error(error);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
 };
