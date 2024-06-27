d2011e5f20752289cd4f2e6df0ab7f4d514f7972
kidstone <hyuksu0308@naver.com>
2024-06-27 22:46:57 +0900

    Feat: 카카오톡 공유 연동(추후 커스텀 논의 필요)

--- src/components/common/MatchingApplyNavBar.jsx
+++ src/components/common/MatchingApplyNavBar.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { API } from '@utils/api'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import useAuthStore from '@store/authStore'
 
 //images
@@ -14,7 +14,7 @@ const MatchingApplyNavBar = ({version, isLowerBarVisible, id, isLikeProps}) => {
   const [isLike, setIsLike] = useState(false)
   const navigate = useNavigate()
   const setAccessToken = useAuthStore(state => state.setAccessToken)
-
+  const location = useLocation()
   useEffect(() => {
     if (isLikeProps !== undefined) {
       // myProps가 undefined가 아닌 경우에만 state 업데이트
@@ -26,6 +26,12 @@ const MatchingApplyNavBar = ({version, isLowerBarVisible, id, isLikeProps}) => {
   useEffect(() => {
       console.log('isLike is changed', isLike)
   }, [isLike]);
+  useEffect(() => {
+    // 카카오 SDK 초기화
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init('7e7de13b930d7f7fcc8c267d547989b4');
+    }
+  }, []);
   
   const goToDetailComments = () => {
     navigate(`/comment-detail/${id}`)
@@ -107,6 +113,71 @@ const MatchingApplyNavBar = ({version, isLowerBarVisible, id, isLikeProps}) => {
       }
     }
   }
+  const clickShareBtn = () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao
+      console.log(kakao)
+      kakao.Share.sendDefault({
+        objectType: 'feed',
+  content: {
+    title: '오늘의 디저트',
+    description: '아메리카노, 빵, 케익',
+    imageUrl:
+      'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+    link: {
+      mobileWebUrl: 'https://developers.kakao.com',
+      webUrl: 'https://developers.kakao.com',
+    },
+  },
+  itemContent: {
+    profileText: '외대 룸메이트 매칭은 "외개인"',
+    profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+    titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+    titleImageText: '친구가 매칭을 공유했어요!',
+    titleImageCategory: '공유한 친구: 김혁수',
+    items: [
+      {
+        item: 'Cake1',
+        itemOp: '1000원',
+      },
+      {
+        item: 'Cake2',
+        itemOp: '2000원',
+      },
+      {
+        item: 'Cake3',
+        itemOp: '3000원',
+      },
+      {
+        item: 'Cake4',
+        itemOp: '4000원',
+      },
+      {
+        item: 'Cake5',
+        itemOp: '5000원',
+      },
+    ],
+    sum: '총 결제금액',
+    sumOp: '15000원',
+  },
+  social: {
+    likeCount: 10,
+    commentCount: 20,
+    sharedCount: 30,
+  },
+  buttons: [
+    {
+      title: '웹으로 이동',
+      link: {
+        mobileWebUrl: 'https://developers.kakao.com',
+        webUrl: `${window.location.origin}${location.pathname}`,
+      },
+    },
+    
+  ],
+      });
+    }
+  }
 
   return (
     <div className={`filter-section bg-white z-50 fixed bottom-0 flex items-center justify-between gap-[15px] h-[91px] w-[393px] px-[26px]
@@ -124,7 +195,7 @@ const MatchingApplyNavBar = ({version, isLowerBarVisible, id, isLikeProps}) => {
         {version === 'comment' &&
           <button onClick={goToDetailComments} className='whitespace-nowrap w-[22px]'><img src={Comment}/></button>
         }
-        <button className='whitespace-nowrap'><img src={Share}/></button>
+        <button onClick={clickShareBtn} className='whitespace-nowrap'><img src={Share}/></button>
       </div>
       <button className='filter-btn whitespace-nowrap'>매칭신청</button>
     </div>
