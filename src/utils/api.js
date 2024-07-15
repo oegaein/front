d116e1710ffa13fa20736dea7c2b2cef48039815
YeEun Kim <kye1115z@naver.com>
2024-07-15 22:25:27 +0900

    docs: api 주소 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import useAuthStore from '@store/authStore'
+import axios from 'axios';
+import useAuthStore from '@store/authStore';
 
-const accessToken = useAuthStore.getState().accessToken
-axios.defaults.withCredentials = true
+const accessToken = useAuthStore.getState().accessToken;
+axios.defaults.withCredentials = true;
 // axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
 export const API = axios.create({
-  baseURL: 'https://api.oegaein.com:8080',
-  // baseURL: '',
-})
+	baseURL: 'https://api.oegaein.org:8080',
+	// baseURL: '',
+});
