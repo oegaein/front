6f8a80b9270251b529f1efb20ba52b98d91ad0f6
YeEun Kim <kye1115z@naver.com>
2024-07-15 22:25:40 +0900

    docs: api 주소 수정

--- src/services/api/MatchingPostAPI.jsx
+++ src/services/api/MatchingPostAPI.jsx
@@ -4,10 +4,7 @@ import axios from 'axios';
 
 export const postMatchingPostAPI = async (submitData, setAccessToken) => {
 	try {
-		const { data } = await API.post(
-			`https://api.oegaein.com:8080/api/v1/matchingposts`,
-			submitData,
-		);
+		const { data } = await API.post(`/api/v1/matchingposts`, submitData);
 		return data;
 	} catch (error) {
 		console.error(error);
@@ -42,9 +39,7 @@ export const postMatchingPostAPI = async (submitData, setAccessToken) => {
 
 export const getMatchingPostAPI = async (matchingpostID, setAccessToken) => {
 	try {
-		const { data } = await API.get(
-			`https://api.oegaein.com:8080/api/v1/matchingposts/${matchingpostID}`,
-		);
+		const { data } = await API.get(`/api/v1/matchingposts/${matchingpostID}`);
 		return data;
 	} catch (error) {
 		console.error(error);
@@ -77,8 +72,6 @@ export const getMatchingPostAPI = async (matchingpostID, setAccessToken) => {
 };
 
 export const getMatchingListAPI = async () => {
-	const { data } = await axios.get(
-		`http://34.64.153.69:8080/api/v1/matchingposts`,
-	);
+	const { data } = await axios.get(`/api/v1/matchingposts`);
 	return data;
 };
