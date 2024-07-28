8b8321163fb9142114d1ade5055818e8938a799b
YeEun Kim <kye1115z@naver.com>
2024-07-29 03:39:39 +0900

    feat: BasicModal 생성

--- src/components/common/modal/BasicModal.jsx
+++ src/components/common/modal/BasicModal.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import { DropdownBackground } from '@common/dropdown/BasicDropdown';
 import COLOR from '@styles/color';
+import FONT from '@styles/fonts';
 
-const BasicModal = ({ isOpen, setIsOpen, children }) => {
+const BasicModal = ({ ver = 'basic', isOpen, setIsOpen, children }) => {
+	const handleClick = () => {
+		setIsOpen(false);
+		window.location.reload();
+	};
 	return (
 		<>
 			<DropdownBackground open={isOpen} onClick={() => setIsOpen(false)} />
-			<BasicModalStyle>{children}</BasicModalStyle>
+			<BasicModalStyle>
+				<p className="title">Error</p>
+				<div className="mb-5">{children}</div>
+				<button className="button" onClick={handleClick}>
+					닫기
+				</button>
+			</BasicModalStyle>
 		</>
 	);
 };
@@ -15,9 +26,10 @@ const BasicModal = ({ isOpen, setIsOpen, children }) => {
 export default BasicModal;
 
 const BasicModalStyle = styled.div`
-	width: fit-content;
-	height: fit-content;
-	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	width: 267px;
+	padding: 20px 30px;
 	position: absolute;
 	top: 50%;
 	left: 50%;
@@ -25,5 +37,21 @@ const BasicModalStyle = styled.div`
 	background-color: ${COLOR.white};
 	border: 1px solid ${COLOR.gray200};
 	border-radius: 15px;
-	z-index: 10;
+	z-index: 60;
+	text-align: start;
+
+	.title {
+		font: ${FONT.title1B23};
+		color: ${COLOR.red};
+		margin-bottom: 15px;
+	}
+
+	.button {
+		width: 100%;
+		height: 40px;
+		background-color: ${COLOR.gray100};
+		color: ${COLOR.black};
+		border-radius: 10px;
+		font: ${FONT.buttonSB15};
+	}
 `;
