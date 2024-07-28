54d1534b242a6860f45b5e7eec57b6f9f05fbabb
YeEun Kim <kye1115z@naver.com>
2024-07-29 02:20:07 +0900

    feat: 유저에 따라 차단하기, 수정하기 적용

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, Link, useNavigate, useLocation } from 'react-router-dom';
 import { API } from '@utils/api';
 import useAuthStore from '@store/authStore';
 import useLowerBarVisible from '@hooks/useLowerBarVisible';
@@ -17,17 +17,26 @@ import ProfileImageContainer from '@components/UserPage/ProfileImageContainer';
 import RoommateReviewList from '@components/UserPage/RoommateReviewList';
 import UserPageInfo from '@components/UserPage/UserPageInfo';
 import UserLifeStyles from '@components/UserPage/UserLifeStyles';
-import { profileData } from 'mocks/api/data/profileData';
+import useMyInfoStore from '@store/myInfoStore';
 import OptionModal from '@common/modal/OptionModal';
+import ConfirmModal from '@common/modal/ConfirmModal';
 import MatchingApplyNavBar from '@common/MatchingApplyNavBar';
+import { BlockUserAPI } from 'services/api/ProfileAPI';
 
 const UserPage = () => {
+	const location = useLocation();
+	console.log(location.pathname);
+	const myname = useMyInfoStore.getState().myInfo.name;
 	const navigate = useNavigate();
-	const setAccessToken = useAuthStore((state) => state.setAccessToken);
-	// const accessToken = useAuthStore.getState().accessToken
-	// const [userInfo, setUserInfo] = useState(profileData)
 	const [userInfo, setUserInfo] = useState({});
-	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [threedots, setThreedots] = useState(false);
+	const [confirm, setConfirm] = useState(false);
+	const [confirmContent, setConfirmContent] = useState({
+		id: -1,
+		msg: '',
+		btn: '',
+		func: () => {},
+	});
 	const isLowerBarVisible = useLowerBarVisible();
 	let { memberId } = useParams();
 	const fetchUserInfoData = async () => {
@@ -51,15 +60,39 @@ const UserPage = () => {
 		}
 	};
 	const handleClickDotsBtn = () => {
-		setIsModalOpen(true);
+		setThreedots(true);
 	};
-	const blockUser = async (blocked_id) => {
-		const response = await API.post('/api/v1/member/block', {
-			blocked_id,
-		});
-		//완료되었는지 확인하는 로직 필요
-		alert('유저를 차단하였습니다.');
+
+	const EditFunc = () => {
+		navigate('/my-profile/edit');
 	};
+
+	const BlockFunc = () => {
+		setConfirm(true);
+		setConfirmContent((prev) => ({
+			...prev,
+			msg: `${userInfo.name}님을 차단할까요?`,
+			btn: '차단',
+			func: async () => {
+				// const res = await BlockUserAPI(data.author_profile.id);
+				// console.log(res);
+			},
+		}));
+	};
+
+	const myOptions = [
+		{
+			content: '수정하기',
+			func: EditFunc,
+		},
+	];
+	const yourOption = [
+		{
+			content: '차단하기',
+			func: BlockFunc,
+		},
+	];
+
 	useEffect(() => {
 		if (memberId === 'my-profile') {
 			fetchMyInfoData();
@@ -69,55 +102,76 @@ const UserPage = () => {
 	}, []);
 
 	return (
-		<SettingStyle>
-			{isModalOpen && (
+		<>
+			{threedots && (
 				<OptionModal
-					options={[{ content: '차단하기', func: () => blockUser(memberId) }]}
-					isOpen={isModalOpen}
-					setIsOpen={setIsModalOpen}
+					options={userInfo.name === myname ? myOptions : yourOption}
+					isOpen={threedots}
+					setIsOpen={setThreedots}
 				/>
 			)}
-			<div className="header px-[28px]">
-				<Header
-					backPath="/roommate"
-					rightContent={Dots}
-					rightEvent={handleClickDotsBtn}
-				>
-					<span className="header-text">{userInfo.name} 님의 프로필</span>
-				</Header>
-			</div>
-      <ProfileImageContainer introduction={userInfo.introduction} profileImage={userInfo.photo_url}/>
-      <div className='userinfolifestyles flex flex-col gap-[10px] border-b border-[#DEDEDE]'>
-        <div className='bg-white'>
-          <div className='user-info pt-[103px] pb-[24px]  px-[25px]'>
-            <h1 className='information-title text-left mb-[16px]'>기본 정보</h1>
-            <UserPageInfo userInfo={userInfo}/>
-          </div>
-          <div className='px-[25px] py-[24px]'>
-            <h1 className='pb-[19px] text-left font-bold'>성향 및 라이프 스타일</h1>
-            <UserLifeStyles userInfo={userInfo}/>
-          </div>
-        </div>
-        <div>
-          <div className='bg-white pt-[24px] px-[24px] flex justify-between items-center'>
-            <h1 className='roommate-review-title'>받은 룸메이트 후기 2</h1>
-            <Link to='reviews' className='more flex'>
-              더보기
-              <img src={Next} alt='see more icon'/>
-            </Link>
-          </div>
-          <div className='bg-white py-[16px]'>
-            <RoommateReviewList memberId={memberId}/>
-          </div>
-        </div>
-      </div>
-      {memberId !== 'my-profile' &&
-      <MatchingApplyNavBar version={'userPage'} isLowerBarVisible={isLowerBarVisible} id={memberId} userInfo={userInfo}
-      fetchUserInfoData={fetchUserInfoData}/>
-      }
-    </SettingStyle>
-  )
-}
+			{confirm && (
+				<ConfirmModal
+					content={confirmContent}
+					isOpen={confirm}
+					setIsOpen={setConfirm}
+				/>
+			)}
+			<SettingStyle>
+				<div className="header px-[28px]">
+					<Header
+						backPath="/roommate"
+						rightContent={Dots}
+						rightEvent={handleClickDotsBtn}
+					>
+						<span className="header-text">{userInfo.name} 님의 프로필</span>
+					</Header>
+				</div>
+				<ProfileImageContainer
+					introduction={userInfo.introduction}
+					profileImage={userInfo.photo_url}
+				/>
+				<div className="userinfolifestyles flex flex-col gap-[10px] border-b border-[#DEDEDE]">
+					<div className="bg-white">
+						<div className="user-info pt-[103px] pb-[24px]  px-[25px]">
+							<h1 className="information-title text-left mb-[16px]">
+								기본 정보
+							</h1>
+							<UserPageInfo userInfo={userInfo} />
+						</div>
+						<div className="px-[25px] py-[24px]">
+							<h1 className="pb-[19px] text-left font-bold">
+								성향 및 라이프 스타일
+							</h1>
+							<UserLifeStyles userInfo={userInfo} />
+						</div>
+					</div>
+					<div>
+						<div className="bg-white pt-[24px] px-[24px] flex justify-between items-center">
+							<h1 className="roommate-review-title">받은 룸메이트 후기 2</h1>
+							<Link to="reviews" className="more flex">
+								더보기
+								<img src={Next} alt="see more icon" />
+							</Link>
+						</div>
+						<div className="bg-white py-[16px]">
+							<RoommateReviewList memberId={memberId} />
+						</div>
+					</div>
+				</div>
+				{memberId !== 'my-profile' && (
+					<MatchingApplyNavBar
+						version={'userPage'}
+						isLowerBarVisible={isLowerBarVisible}
+						id={memberId}
+						userInfo={userInfo}
+						fetchUserInfoData={fetchUserInfoData}
+					/>
+				)}
+			</SettingStyle>
+		</>
+	);
+};
 
 export default UserPage;
 
