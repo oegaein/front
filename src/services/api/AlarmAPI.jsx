8c83350b2faf1c72379d200a72c2a9b54750b9ce
YeEun Kim <kye1115z@naver.com>
2024-08-09 01:22:08 +0900

    feat: 알람 api 에러처리

--- src/services/api/AlarmAPI.jsx
+++ src/services/api/AlarmAPI.jsx
@@ -2,11 +2,12 @@ import { makeAuthorizedRequest } from '@utils/makeAuthorizedRequest';
 import { toast } from 'react-toastify';
 export const getAlarmAPI = async () => {
 	const response = await makeAuthorizedRequest(`/api/v1/roommate-alarms`);
-	if (response.status === 200) {
-		return response;
-	} else {
-		toast.error(response.data.errorMsg);
+	if (response === undefined) {
 		return;
+	} else {
+		if (response.status === 200) {
+			return response;
+		}
 	}
 };
 
@@ -15,11 +16,13 @@ export const deleteAllAlarmAPI = async () => {
 		`/api/v1/roommate-alarms`,
 		'delete',
 	);
-	if (response.status === 204) {
-		return true;
+	if (response === undefined) {
+		toast.error('알림을 삭제하지 못했습니다!');
+		return;
 	} else {
-		toast.error(response.data.errorMsg);
-		return false;
+		if (response.status === 204) {
+			return response;
+		}
 	}
 };
 
@@ -28,10 +31,12 @@ export const deleteAlarmAPI = async (id) => {
 		`/api/v1/roommate-alarms/${id}`,
 		'delete',
 	);
-	if (response.status === 204) {
-		return true;
+	if (response === undefined) {
+		toast.error('알림을 삭제하지 못했습니다!');
+		return;
 	} else {
-		toast.error(response.data.errorMsg);
-		return false;
+		if (response.status === 204) {
+			return response;
+		}
 	}
 };
