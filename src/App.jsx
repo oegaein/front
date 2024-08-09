751554fadf4c9db4c719934a295ef4c635216fdd
김혁수 <hyuksu0308@gmail.com>
2024-08-09 10:53:54 +0900

    Fix: App.jsx / 경로에도 HomePage 리턴

--- src/App.jsx
+++ src/App.jsx
@@ -55,8 +55,9 @@ function App() {
 				<Router>
 					<Routes>
 						<Route path="/" element={<MainLayout />}>
+							<Route index element={<HomePage />} />
 							<Route path="callback" element={<CallBack />} />
-							<Route index path="home" element={<HomePage />} />
+							<Route path="home" element={<HomePage />} />
 							<Route path="home/dormnews" element={<DormNewsPage />} />
 							<Route path="home/ending-soon" element={<EndingsoonPage />} />
 							<Route
